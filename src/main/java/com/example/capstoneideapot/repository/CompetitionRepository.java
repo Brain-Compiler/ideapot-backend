34e1676a573c6e83240d83c3c07635bd953331f7
Daehyeong Park <85856074+eoguddl@users.noreply.github.com>
2022-10-29 21:55:02 +0900

    Delete src directory

--- src/main/java/com/example/capstoneideapot/repository/CompetitionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.capstoneideapot.repository;
-
-import com.example.capstoneideapot.entity.Competition;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompetitionRepository extends JpaRepository<Competition, Long> {
-}
