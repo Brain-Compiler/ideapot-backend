8f1e6a366d1c9fbfd7c5818c68a0a3fea6e455ed
Daehyeong Park <85856074+eoguddl@users.noreply.github.com>
2022-10-29 11:47:32 +0900

    Delete src directory

--- src/main/java/com/example/capstoneideapot/repository/FilesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.capstoneideapot.repository;
-
-import com.example.capstoneideapot.entity.Files;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilesRepository extends JpaRepository<Files, Long> {
-}
